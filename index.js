4b9b3cd799f294bc9f29c18488cc5f024a95de76
sudipnag1 <sudipnag1@ece.iitkgp.ac.in>
2021-12-29 20:11:20 +0530

    m

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function signin(event){
             // console.log(user)
             console.log("Signed in")
             document.querySelector(".loader-signin").style.display = "none";
-            location.href = "/AQM_Website"    // For running on the webpage
+            location.href = "/NeuralStimulator"    // For running on the webpage
             //location.href = "/" // For running in a local PC
             // document.getElementsByClassName("text")[0].style.display = "block";
         })
